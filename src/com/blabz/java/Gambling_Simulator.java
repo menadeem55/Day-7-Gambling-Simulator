5119cab7749f7b78cfc6330dadd342de664bf70c
me.nadeem55@gmail.com <me.nadeem55@gmail.com>
2022-10-05 16:06:32 +0530

    [Added: Initial Stake Amount and bet Amount Programming code]

--- src/com/blabz/java/Gambling_Simulator.java
+++ src/com/blabz/java/Gambling_Simulator.java
@@ -1,8 +1,16 @@
 package com.blabz.java;
 
 public class Gambling_Simulator {
+
+	public static final int initialStake = 100;
+	public static final int stakeBet = 1;
+
 	public static void main(String[] args) {
+
 		System.out.println("Welcome to the Gambling Simulator Game");
+		System.out.println("Initial Stake is " + initialStake + "$");
+		System.out.println("Stake Bet is " + stakeBet + "$");
+
 	}
 
 }
