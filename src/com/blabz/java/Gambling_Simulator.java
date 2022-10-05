41beee57d7c8473099d5f82ccf9a189507455dd2
me.nadeem55@gmail.com <me.nadeem55@gmail.com>
2022-10-05 16:14:04 +0530

    [Added: Calculate stake after winning or loosing logic]

--- src/com/blabz/java/Gambling_Simulator.java
+++ src/com/blabz/java/Gambling_Simulator.java
@@ -1,16 +1,32 @@
 package com.blabz.java;
 
+import java.util.Random;
+
 public class Gambling_Simulator {
 
 	public static final int initialStake = 100;
 	public static final int stakeBet = 1;
+	static Random random = new Random();
 
 	public static void main(String[] args) {
 
-		System.out.println("Welcome to the Gambling Simulator Game");
-		System.out.println("Initial Stake is " + initialStake + "$");
-		System.out.println("Stake Bet is " + stakeBet + "$");
+		int totalStake = initialStake;
 
+		System.out.println("Welcome to the Gambling Simulator Game");
+
+		int play = random.nextInt(2);
+		switch (play) {
+
+		case 0:
+			totalStake = totalStake - stakeBet;
+			System.out.println("Stake after losing: " + totalStake);
+			break;
+
+		case 1:
+			totalStake = totalStake + stakeBet;
+			System.out.println("Stake after winning: " + totalStake);
+			break;
+		}
 	}
 
 }
